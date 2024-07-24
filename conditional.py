0b3db6e799912b18f47aeb14401db46a8415f12f
fatehullah778866 <fatehmsd2522004@gmail.com>
2024-07-23 20:52:01 -0700

    conditional commited in python

---
+++ conditional.py
@@ -0,0 +1,8 @@
+number1=int(input("Enter the Number_1="))
+number2=int(input("Enter the Number_2="))
+if number1>number2:
+    print("number1 is greater than number2")
+elif number1==number2:
+    print("number1 is equal to number2")
+else:
+    print("number1 is less than number2")
